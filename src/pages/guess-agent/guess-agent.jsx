d82ebc78bd8ee2cebf16fd677c40afda6f11d0b8
ErrazIG <frederic-vml@hotmail.fr>
2024-05-28 16:20:04 +0200

    guess agent

---
+++ src/pages/guess-agent/guess-agent.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AutoCompleteInput from "../../components/searchbar/searchbar";
+import style from "./guess-agent.module.css";
+
+const GuessAgentPage = () => {
+  const [agents, setAgents] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const fetchAgents = async () => {
+      try {
+        const response = await axios.get("https://valorant-api.com/v1/agents");
+        if (response.status === 200) {
+          console.log(response.data); // Affichez les données reçues pour vérifier leur structure
+          if (Array.isArray(response.data.data)) {
+            // Assurez-vous d'accéder à la propriété data du tableau
+            const agentNames = response.data.data
+              .filter((agent) => agent && agent.displayName) // Filtrer les objets qui ont la propriété displayName
+              .map((agent) => agent.displayName);
+            console.log(agentNames);
+            setAgents(agentNames);
+            setSuggestions(agentNames); // Vous pouvez appliquer des filtres ou des conditions ici
+          } else {
+            console.error(
+              "La réponse de l'API n'est pas un tableau:",
+              response.data
+            );
+          }
+        }
+      } catch (error) {
+        console.error("Erreur lors de la récupération des agents:", error);
+      }
+    };
+
+    fetchAgents();
+  }, []);
+
+  return (
+    <>
+      <div className={style.gameAgent}>
+        <h2 className={style.gameTitle}>Guess The Agent</h2>
+        <AutoCompleteInput suggestions={suggestions} />
+      </div>
+    </>
+  );
+};
+
+export default GuessAgentPage;
